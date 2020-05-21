eea4633e5d898449e9cd91d7b2ced6f344b66475
nickKBI <nick.guinand@knowledgebridgeintl.com>
2020-05-21 12:12:35 -0400

    Added base connect 4 game. Missing win condition

---
+++ connectFourLogic.js
@@ -0,0 +1,110 @@
+//var player1 = prompt("Enter Player One Name, You will be Blue");
+var player1Color = "rgb(86, 151, 255)";
+
+//var player2 = prompt("Enter Player Two Name, You will be Red");
+var player2Color = "rgb(237, 45, 73)";
+var player1Active = true;
+var inactiveColor = "rgb(187, 187, 187)"
+var elementColIndex;
+var buttonRelease = true;
+
+
+
+$('button').mouseenter(function(){
+    if($(this).attr('class') == 'start' && $(this).css('background-color') =='rgb(187, 187, 187)' ){
+    $(this).css('background-color',"rgb(153,153,0");
+    }
+})
+
+$('button').mouseleave(function(){
+    if($(this).css('background-color') === "rgb(153, 153, 0)"){
+        $(this).css('background-color',"rgb(187, 187, 187)");
+
+    }
+})
+
+
+
+$('button').click(function(){
+    // console.log($(this).attr('class'));
+    elementColIndex = $('.start').index($(this));
+    // console.log(elementColIndex);
+    //Because we changed the background color to yellow, we have to move our mouse away to keep clicking
+    if($(this).css('background-color') == 'rgb(153, 153, 0)' && $(this).attr('class') == 'start' && buttonRelease){
+        
+        //This async function is required to lock down multiple button clicks
+        buttonRelease = false;
+        async function asyncCall() {
+            // console.log('calling');
+            const result = await buttonResolver();
+            // console.log(result);
+            // expected output: 'resolved'
+          }
+        function buttonResolver() {
+            return new Promise(resolve => {
+              setTimeout(() => {
+                resolve('resolved');
+                buttonRelease =true;
+              }, 1700);
+            });
+          }
+          asyncCall();
+
+        if(player1Active){
+            player1Active = false;
+            cascadeDown($(this), elementColIndex, player1Color);
+            // $(this).css('background-color',player1Color);
+        }
+        else{
+            // $(this).css('background-color',player2Color);
+            player1Active=true;
+            cascadeDown($(this), elementColIndex, player2Color);
+        }
+    }
+    else{
+        console.log('else')
+        console.log($(this).css('background-color') == 'rgb(153, 153, 0)');
+        console.log($(this).attr('class') == 'start');
+        console.log(buttonRelease);
+    }
+})
+
+function cascadeDown(element, index, playerColor){
+    buttonRelease=false;
+    $('tr').each(function(count){
+        //We get the rows going down, get the button at that index and go grab that button
+        let row = this;
+        let colSize= row.cells.length
+        // console.log(count);
+        console.log(row);
+        // console.log(row.cells);
+        // console.log(row.cells[index])
+        // console.log(row.cells[index].children)
+        // console.log(row.cells[index].children.button)
+        let button = row.cells[index].children.button;
+        // let buttonColor = $(button).css('background-color')
+        console.log(element)
+        
+        try{
+            console.log('try')
+            let nextCell = $('tr')[count+1].cells[index].children.button
+            // console.log(nextCell);
+            if($(nextCell).css('background-color') == "rgb(187, 187, 187)"){
+                setTimeout(function(){
+                    $(nextCell).css('background-color', playerColor)
+                $(button).css('background-color', 'rgb(187, 187, 187)')
+
+                }, 300*count)
+            }
+            else {
+                $(element).css('background-color', playerColor);
+            }
+        }
+        catch(err){
+            // $(button).css('background-color', playerColor)
+            console.log(err)
+        }
+        
+    })
+}
+
